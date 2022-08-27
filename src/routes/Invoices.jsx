8a91f92264ca823e3f5a71e2511b8d6782228cb6
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-08-27 14:49:07 -0400

    add data nad list invoices

--- src/routes/Invoices.jsx
+++ src/routes/Invoices.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import { getAllInvoices, getInvoice } from "../data"
 
 export default function Invoices() {
+
   return (
     <section style={{ padding: "1rem 0" }}>
       <h2>Invoices</h2>
+      <ol>
+        {
+          getAllInvoices().map(invoice =>
+            <li key={invoice.number}>
+              <Link to={`/invoices/${invoice.number}`}>{invoice.name}</Link>
+            </li>
+          )
+        }
+      </ol>
+      
     </section>
   );
 }
