a3e5a284c32dfa8ec6b2d305ca7beb8cc57eb8cc
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-08-27 08:09:43 -0400

    add initial project cleanup

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App data={'a'}/>
-    {
-      console.log(<App />)
-    }
+    <App />
   </React.StrictMode>
 );
 
