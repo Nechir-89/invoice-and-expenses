714d354c1fb93c5ed59c0d77a230f6cec5d53ca7
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-09-01 15:34:04 -0400

    add filtring to invoices

--- src/routes/Invoices.jsx
+++ src/routes/Invoices.jsx
@@ -1,15 +1,33 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getAllInvoices, getInvoice } from "../data"
 
 export default function Invoices() {
-
+  const [searchParam, setSearchParam] = useSearchParams();
+  console.log(searchParam);
   return (
     <>
       <section style={{ padding: "1rem 0" }}>
+        <div>
+          <label>
+            Invoice
+            <input
+              type="text"
+              placeholder="type invoice name"
+              onChange={(e) => {
+                let searchInput = e.target.value;
+                setSearchParam({ invoice: searchInput })
+              }}
+              value={searchParam.get("invoice") || ""}
+            />
+          </label>
+        </div>
         <h2>Invoices</h2>
         <ol>
           {
-            getAllInvoices().map(invoice =>
+            getAllInvoices().filter(invoice => {
+              let queryParam = searchParam.get("invoice").toLowerCase();
+              return invoice.name.toLowerCase().includes(queryParam);
+            }).map(invoice =>
               <li key={invoice.number}>
                 <NavLink
                   to={`/invoices/${invoice.number}`}
