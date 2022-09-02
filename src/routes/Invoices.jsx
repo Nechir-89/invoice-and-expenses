465fbf757bc57bf81a57427fed027a1c8db6c0ae
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-09-02 13:06:48 -0400

    filter invoices based on different inputs

--- src/routes/Invoices.jsx
+++ src/routes/Invoices.jsx
@@ -4,21 +4,47 @@ import { getAllInvoices, getInvoice } from "../data"
 export default function Invoices() {
   const [searchParam, setSearchParam] = useSearchParams();
   let location = useLocation();
-  console.log(searchParam);
   return (
     <>
       <section style={{ padding: "1rem 0" }}>
-        <div>
+        <div style={{ marginBottom: "0.5rem" }}>
           <label>
-            Invoice
+            Invoice:
             <input
               type="text"
               placeholder="type invoice name"
               onChange={(e) => {
                 let searchInput = e.target.value;
-                setSearchParam({ invoice: searchInput })
+                let query = {}
+                if (searchInput)
+                  query.invoice = searchInput
+                if (searchParam.get("invoiceId"))
+                  query.invoiceId = searchParam.get("invoiceId");
+
+                setSearchParam(query);
               }}
               value={searchParam.get("invoice") || ""}
+            />
+          </label>
+        </div>
+        <div style={{ marginBottom: "0.5rem" }}>
+          <label>
+            Invoice ID:
+            <input
+              type="text"
+              placeholder="type invoice ID"
+              value={searchParam.get("invoiceId") || ""}
+              onChange={(e) => {
+                let searchInput = e.target.value;
+                let query = {};
+                if (searchInput)
+                  query.invoiceId = searchInput
+                if (searchParam.get("invoice"))
+                  query.invoice = searchParam.get("invoice");
+                setSearchParam(query)
+                // searchParam.append("invoiceId", searchInput);
+                // console.log(searchParam.getAll());
+              }}
             />
           </label>
         </div>
@@ -26,11 +52,15 @@ export default function Invoices() {
         <ol>
           {
             getAllInvoices().filter(invoice => {
-              let queryParam = searchParam.get("invoice");
-              if (queryParam)
-                return invoice.name.toLowerCase().includes(queryParam.toLowerCase());
+              let queryParamInvoice = searchParam.get("invoice");
+              let queryParamInvoiceId = searchParam.get("invoiceId");
 
-              return true;
+              return queryParamInvoice && queryParamInvoiceId ?
+                invoice.name.toLowerCase().includes(queryParamInvoice.toLowerCase()) && String(invoice.number).includes(queryParamInvoiceId)
+                : queryParamInvoice ?
+                  invoice.name.toLowerCase().includes(queryParamInvoice.toLowerCase())
+                  : queryParamInvoiceId ? String(invoice.number).includes(queryParamInvoiceId)
+                    : true
             }).map(invoice =>
               <li key={invoice.number}>
                 <NavLink
